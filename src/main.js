eebd8734ccd03fe90d998eea8e6ebe9c3789fdbc
joshua <jlewin@bksteamcenter.org>
2025-06-03 14:55:58 -0400

    i tried

---
+++ src/main.js
@@ -0,0 +1,9 @@
+let points = 0
+function addPoint() {
+    points = points + 1
+    console.log('points')
+    document.getElementById("counter").innerText = "" + points ;
+}
+function buyBeetRoot(){
+    if 
+}
